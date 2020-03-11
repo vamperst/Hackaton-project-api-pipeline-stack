1725312f1bbfeff2ebb0d647f9638e82a2b6a798
Barbosa <rafbarbo@8c85902385c3.ant.amazon.com>
2020-03-11 09:31:39 -0300

    first commit

---
+++ handler.py
@@ -0,0 +1,29 @@
+import json
+
+import boto3
+
+
+import os
+grupo = os.environ['GROUP']
+
+codepipeline = boto3.client('codepipeline')
+
+def main(event, context):
+    print(str(event))
+    print(str(context))
+    
+    
+    responsePipeline = codepipeline.start_pipeline_execution(
+        name='hackaton-cicd-deploy',
+    )
+    
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(responsePipeline)
+    }
+    
+    return response
+    
+    
+   
+    
